07da59ca4d9d1f76e78dc858866867f0ad086310
SteffenHamann <steffen.hamann@yahoo.de>
2017-03-25 20:32:59 +0100

    Unterschied zwischen HTML, PHP, HTML + PHP

---
+++ HelloWorld3.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+
+
+<html>                                          <!-- Start HTML teil-->
+    <head> 
+        <title>Test Internet Page</title>
+    </head>
+
+    <body>                                      <!-- Ende HTML Teil-->
+
+        <?php                                   // Ende HTML Teil
+                                     
+            echo "<h1>Hello World</h1>";
+                                                // Ende des PHP Teil
+        ?>                                     
+                                                <!-- Start HTML teil-->
+        
+    </body>                                     
+
+</html>                                         <!-- Ende HTML Teil-->
